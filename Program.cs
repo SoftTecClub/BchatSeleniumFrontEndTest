85bbc8f050c2e3c5242a4df921b88885ae9ff09e
Ryokugyoku <mashumaro1997@icloud.com>
2024-09-29 15:36:07 +0900

    #1  デフォルトのプロジェクト作成

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BchatSeleniumFrontEndTest;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+
+/// <summary>
+/// The main entry point for the application.
+/// </summary>
+/// <date>2024/09/29</date>
+/// <author>Ryokugyoku</author>
+public class Program()
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
